725dc4749aed175980fec03f5b16c2ad2d4a7d09
Verity Carlos <verity.carlos@gmail.com>
2022-04-13 17:19:45 -0500

    commit tree 4dc62bb40c653023c89a67481991456bbba4f1fb

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) { 
+    let blockDifference = 42 - blockNumber; 
+    return blockDifference < 0 ? blockDifference * -1 : blockDifference;  
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let feetBetweenBlocks = (destination-start)*264;
+    return feetBetweenBlocks < 0 ? feetBetweenBlocks * -1 : feetBetweenBlocks; 
+}
+
+function calculatesFarePrice(start, destination) {
+    let  distance = distanceTravelledInFeet(start, destination);
+    if (distance <=400) {
+        return 0;
+    } else if (distance >= 2500) {
+        return 'cannot travel that far';
+    } else if (distance > 2000) {
+        return 25;
+    } else if (distance > 400) {
+        return (distance-400) * 0.02;
+    }
+}
